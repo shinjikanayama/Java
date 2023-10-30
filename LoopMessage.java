944ad2d7690e331c90c9f5a62bfd5e6a53f24d1f
shinjikanayama <sinzi.1202@gmail.com>
2023-10-30 11:12:46 +0000

    [練習2]

---
+++ LoopMessage.java
@@ -0,0 +1,10 @@
+class LoopMessage{
+  public static void main(String[] args){
+
+     int i = 0;
+    while( i < 11 ){
+      System.out.println(i + "回目：フリーラーニングを広めたい！");
+      i++;
+    }
+  }
+}
