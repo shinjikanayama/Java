18c9b4f609285f7aa71b4492a3845d59250f80c6
shinjikanayama <sinzi.1202@gmail.com>
2023-12-07 13:18:57 +0000

    [課題1]

---
+++ Car.java
@@ -0,0 +1,20 @@
+class Car{
+  //変数
+  int no;
+  int speed;
+
+  //メソッド
+  void setNo(int n){
+    no = n;
+  }
+  void run(int s){
+    speed = s;
+  }
+  void stop(){
+    speed = 0;
+  }
+  void display(){
+    System.out.println("ナンバー" + no + "の速度は" + speed + "です");
+  }
+}
+
